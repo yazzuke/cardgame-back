e496d9a6be16ac413a044caed7f864c1bf30f8d6
Felipe Avila <juanfe.avilat@uniagustiniana.edu.co>
2024-02-04 16:43:50 -0500

    modding cors configuration

--- src/main/java/com/cards/game/controller/CorsConfiguration.java
+++ src/main/java/com/cards/game/controller/CorsConfiguration.java
@@ -12,7 +12,7 @@ public class CorsConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/gamecard/**")
+                registry.addMapping("/**")
                         .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
